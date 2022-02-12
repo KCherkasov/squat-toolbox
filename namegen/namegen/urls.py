cbf46627e44949055c4494ebec5eba90639f94b6
Kirill Cherkasov <kvcherk@live.ru>
2022-02-12 18:30:31 +0300

    add new url

--- namegen/namegen/urls.py
+++ namegen/namegen/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from . import views, settings
 
 urlpatterns = [
+    path('/', views.index, name='main'),
     path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
