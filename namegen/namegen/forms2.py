1d2e180726eda1b336c46834f01dd48e523e684b
Kirill Cherkasov <kvcherk@live.ru>
2022-02-21 03:08:57 +0300

    Mixed language generation added

--- namegen/namegen/forms2.py
+++ namegen/namegen/forms2.py
@@ -2,8 +2,7 @@
 
 from django import forms
 
-
-class NamegenForm(forms.Form):
+class Constants:
     SCAND = 'SC'
     LATIN = 'LAT'
     SPAIN = 'SP'
@@ -16,15 +15,17 @@ class NamegenForm(forms.Form):
     NOBLE = 'N'
     SIMPLE = 'S'
 
-    LANGS = (['SC', u'Скандинавия'], ['LAT', u'Латынь'], ['SP', u'Испания'], ['IT', u'Италия'], )
+
+class NamegenForm(forms.Form):
+    LANGS = (['SC', u'Скандинавия'], ['LAT', u'Латынь'], ['SP', u'Испания'], ['IT', u'Италия'], ['R', u'Микс'], )
 
     GENDERS = (['M', u'Мужской'], ['F', u'Женский'], ['R', u'Случайно'],)
 
     NOBILITY = (['N', u'Благородный'], ['S', u'Простолюдин'], ['R', u'Случайно'], )
 
-    lang = forms.ChoiceField(label=u'Языковая группа', required=True, choices=LANGS, initial=SCAND)
-    gender = forms.ChoiceField(label=u'Пол', required=True, choices=GENDERS, initial=RANDOM)
-    nobility = forms.ChoiceField(label=u'Благородный', required=True, choices=NOBILITY, initial=RANDOM)
+    lang = forms.ChoiceField(label=u'Языковая группа', required=True, choices=LANGS, initial=Constants.RANDOM)
+    gender = forms.ChoiceField(label=u'Пол', required=True, choices=GENDERS, initial=Constants.RANDOM)
+    nobility = forms.ChoiceField(label=u'Благородный', required=True, choices=NOBILITY, initial=Constants.RANDOM)
     count = forms.IntegerField(localize=False, min_value=1, max_value=100000, required=True, initial=1,
                                widget=forms.NumberInput(attrs={'class': 'form-control',
                                                                'placeholder': u'Количество имен', }),
--- namegen/namegen/views.py
+++ namegen/namegen/views.py
@@ -6,16 +6,19 @@ from django.shortcuts import render, reverse
 
 from .models import MaleScand, FemaleScand, MaleLatin, FemaleLatin, CognomenLatin,\
     MaleSpanish, FemaleSpanish, MaleItalian, FemaleItalian, SurnamesItalian
-from .forms2 import NamegenForm
+from .forms2 import NamegenForm, Constants
 
 import random
 
-version = '1.3.1'
+version = '1.4.0'
 
 CONSONANTS = 'бвгджзйклмнпрстфхшщчц'
 RIGHT_CONSONANTS = 'йнрс'
 VOWELS = 'аеёиоуыэюя'
 
+
+LANGS = NamegenForm.LANGS
+LANG_IDS = [Constants.SCAND, Constants.LATIN, Constants.SPAIN, Constants.ITALY, ]
 
 def main(request):
     return HttpResponseRedirect(reverse('index'))
@@ -31,7 +34,7 @@ def index(request):
             nobility = data['nobility']
             count = data['count']
             names = list()
-            if lang == 'SC':
+            if lang == Constants.SCAND:
                 first_parts_male = MaleScand.objects.get_first_parts_list()
                 second_parts_male = MaleScand.objects.get_second_parts_list()
 
@@ -44,7 +47,7 @@ def index(request):
                 else:
                     names = generate_names_scand(gender, nobility, first_parts_male, second_parts_male,
                                                  first_parts_female, second_parts_female, count)
-            elif lang == 'LAT':
+            elif lang == Constants.LATIN:
                 first_parts_male = MaleLatin.objects.get_first_parts_list()
                 second_parts_male = MaleLatin.objects.get_second_parts_list()
 
@@ -67,7 +70,7 @@ def index(request):
                                                  first_parts_female, second_parts_female,
                                                  cognomen_first_parts, cognomen_second_parts_male,
                                                  cognomen_second_parts_female)
-            elif lang == 'SP':
+            elif lang == Constants.SPAIN:
                 first_parts_male = MaleSpanish.objects.get_first_parts_list()
                 second_parts_male = MaleSpanish.objects.get_second_parts_list()
 
@@ -82,7 +85,7 @@ def index(request):
                     names = generate_names_spanish(gender, nobility, count,
                                                    first_parts_male, second_parts_male,
                                                    first_parts_female, second_parts_female)
-            elif lang == 'IT':
+            elif lang == Constants.ITALY:
                 first_parts_male = MaleItalian.objects.get_first_parts_list()
                 second_parts_male = MaleItalian.objects.get_second_parts_list()
 
@@ -102,6 +105,22 @@ def index(request):
                                                    first_parts_male, second_parts_male,
                                                    first_parts_female, second_parts_female,
                                                    surnames_first_part, surnames_second_part)
+            elif lang == Constants.RANDOM:
+                [first_parts_male, second_parts_male, first_parts_female, second_parts_female,
+                 cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female,
+                 surname_italian_firsts, surname_italian_seconds] = prepare_data_rand_namegen()
+                if count == 1:
+                    names.append(generate_name_rand(gender, nobility,
+                                                    first_parts_male, second_parts_male,
+                                                    first_parts_female, second_parts_female,
+                                                    cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female,
+                                                    surname_italian_firsts, surname_italian_seconds))
+                else:
+                    names = generate_names_rand(gender, nobility, count,
+                                                first_parts_male, second_parts_male,
+                                                first_parts_female, second_parts_female,
+                                                cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female,
+                                                surname_italian_firsts, surname_italian_seconds)
             return render(request, 'index.html', {'form': form, 'names': names, 'version': version, })
 
     else:
@@ -121,13 +140,13 @@ def generate_name_scand(gender, nobility, first_parts_male, second_parts_male,
                          first_parts_female, second_parts_female)
     name += ' '
     middle_name = ''
-    if nobility == 'N':
+    if nobility == Constants.NOBLE:
         if d100() <= 50:
-            middle_name = generate_name(MaleScand, FemaleScand, 'M',
+            middle_name = generate_name(MaleScand, FemaleScand, Constants.MALE,
                                         first_parts_male, second_parts_male,
                                         first_parts_female, second_parts_female)
         else:
-            middle_name = generate_name(MaleScand, FemaleScand, 'F',
+            middle_name = generate_name(MaleScand, FemaleScand, Constants.FEMALE,
                                         first_parts_male, second_parts_male,
                                         first_parts_female, second_parts_female)
         middle_name += ' '
@@ -157,7 +176,7 @@ def generate_name_latin(gender, nobility,
                          first_parts_female, second_parts_female)
     name += ' '
     middle_name = ''
-    if nobility == 'N':
+    if nobility == Constants.NOBLE:
         middle_name = generate_name(MaleLatin, FemaleLatin, gender,
                                     first_parts_male, second_parts_male,
                                     first_parts_female, second_parts_female)
@@ -201,7 +220,7 @@ def generate_name_spanish(gender, nobility,
                          first_parts_male, second_parts_male,
                          first_parts_female, second_parts_female)
     name += ' '
-    if nobility == 'N':
+    if nobility == Constants.NOBLE:
         threshold = 75
     else:
         threshold = 50
@@ -239,11 +258,11 @@ def generate_name_italian(gender, nobility,
     name += ' '
     for i in range(choice([1, 2, 3])):
         if d100() <= 50:
-            name += generate_name(MaleItalian, FemaleItalian, 'M',
+            name += generate_name(MaleItalian, FemaleItalian, Constants.MALE,
                                   first_parts_male, second_parts_male,
                                   first_parts_female, second_parts_female) + ' '
         else:
-            name += generate_name(MaleItalian, FemaleItalian, 'F',
+            name += generate_name(MaleItalian, FemaleItalian, Constants.FEMALE,
                                   first_parts_male, second_parts_male,
                                   first_parts_female, second_parts_female) + ' '
     surname = generate_italian_surname(nobility, surnames_first_parts, surnames_second_part)
@@ -264,37 +283,139 @@ def generate_names_italian(gender, nobility, count,
     return names
 
 
+def prepare_data_rand_namegen():
+    first_parts_male = dict()
+    first_parts_male[Constants.SCAND] = MaleScand.objects.get_first_parts_list()
+    first_parts_male[Constants.LATIN] = MaleLatin.objects.get_first_parts_list()
+    first_parts_male[Constants.SPAIN] = MaleSpanish.objects.get_first_parts_list()
+    first_parts_male[Constants.ITALY] = MaleItalian.objects.get_first_parts_list()
+
+    second_parts_male = dict()
+    second_parts_male[Constants.SCAND] = MaleScand.objects.get_second_parts_list()
+    second_parts_male[Constants.LATIN] = MaleLatin.objects.get_second_parts_list()
+    second_parts_male[Constants.SPAIN] = MaleSpanish.objects.get_second_parts_list()
+    second_parts_male[Constants.ITALY] = MaleScand.objects.get_second_parts_list()
+
+    first_parts_female = dict()
+    first_parts_female[Constants.SCAND] = FemaleScand.objects.get_first_parts_list()
+    first_parts_female[Constants.LATIN] = FemaleLatin.objects.get_first_parts_list()
+    first_parts_female[Constants.SPAIN] = FemaleSpanish.objects.get_first_parts_list()
+    first_parts_female[Constants.ITALY] = FemaleItalian.objects.get_first_parts_list()
+
+    second_parts_female = dict()
+    second_parts_female[Constants.SCAND] = FemaleScand.objects.get_second_parts_list()
+    second_parts_female[Constants.LATIN] = FemaleLatin.objects.get_second_parts_list()
+    second_parts_female[Constants.SPAIN] = FemaleSpanish.objects.get_second_parts_list()
+    second_parts_female[Constants.ITALY] = FemaleItalian.objects.get_second_parts_list()
+
+    cognomen_firsts = CognomenLatin.objects.get_first_parts_list()
+    cognomen_seconds_male = CognomenLatin.objects.get_second_parts_male_list()
+    cognomen_seconds_female = CognomenLatin.objects.get_second_parts_female_list()
+
+    surname_italian_firsts = SurnamesItalian.objects.get_first_parts_list()
+    surname_italian_seconds = SurnamesItalian.objects.get_second_parts_list()
+
+    return [first_parts_male, second_parts_male, first_parts_female, second_parts_female,
+            cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female,
+            surname_italian_firsts, surname_italian_seconds]
+
+
+def generate_name_rand(gender, nobility,
+                       first_parts_male, second_parts_male,
+                       first_parts_female, second_parts_female,
+                       cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female,
+                       surname_italian_firsts, surname_italian_seconds):
+    [gender, nobility, gender_tail, noble_tail] = resolve_randomness(gender, nobility)
+    name = ''
+    names_count = choice([1, 2, 3, 4])
+    for i in range(names_count):
+        lang = choice(LANG_IDS)
+        [male, female] = determine_lang(lang)
+        name += generate_name(male, female, gender,
+                              first_parts_male.get(lang), second_parts_male.get(lang),
+                              first_parts_female.get(lang), second_parts_female.get(lang)) + ' '
+    surname = ''
+    lang = choice(LANG_IDS)
+    if lang == Constants.SCAND:
+        surname = generate_scand_surname(gender,
+                                         first_parts_male.get(lang), second_parts_male.get(lang),
+                                         first_parts_female.get(lang), second_parts_female.get(lang))
+    elif lang == Constants.LATIN:
+        surname = generate_cognomen(gender, cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female)
+    elif lang == Constants.SPAIN:
+        surname = generate_spanish_surname(nobility,
+                                           first_parts_male.get(lang), second_parts_male.get(lang),
+                                           first_parts_female.get(lang), second_parts_female.get(lang))
+    elif lang == Constants.ITALY:
+        surname = generate_italian_surname(nobility, surname_italian_firsts, surname_italian_seconds)
+    surname + ' '
+    return name + surname + gender_tail + noble_tail
+
+
+def determine_lang(lang):
+    male = None
+    female = None
+    if lang == Constants.SCAND:
+        male = MaleScand
+        female = FemaleScand
+    elif lang == Constants.LATIN:
+        male = MaleLatin
+        female = FemaleLatin
+    elif lang == Constants.SPAIN:
+        male = MaleSpanish
+        female = FemaleSpanish
+    elif lang == Constants.ITALY:
+        male = MaleItalian
+        female = FemaleItalian
+    return [male, female]
+
+
+def generate_names_rand(gender, nobility, count,
+                        first_parts_male, second_parts_male,
+                        first_parts_female, second_parts_female,
+                        cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female,
+                        surname_italian_firsts, surname_italian_seconds):
+    names = list()
+    for i in range(count):
+        names.append(generate_name_rand(gender, nobility,
+                                        first_parts_male, second_parts_male,
+                                        first_parts_female, second_parts_female,
+                                        cognomen_firsts, cognomen_seconds_male, cognomen_seconds_female,
+                                        surname_italian_firsts, surname_italian_seconds))
+    return names
+
+
 def determine_gender(gender):
-    if gender == 'R':
+    if gender == Constants.RANDOM:
         if d100() <= 50:
-            gender = 'M'
+            gender = Constants.MALE
         else:
-            gender = 'F'
+            gender = Constants.FEMALE
     return gender
 
 
 def determine_nobility(nobility):
-    if nobility == 'R':
+    if nobility == Constants.RANDOM:
         if d100() <= 50:
-            nobility = 'N'
+            nobility = Constants.NOBLE
         else:
-            nobility = 'S'
+            nobility = Constants.SIMPLE
     return nobility
 
 
 def resolve_randomness(gender, nobility):
     gender_tail = ''
-    if gender == 'R':
+    if gender == Constants.RANDOM:
         gender = determine_gender(gender)
-        if gender == 'M':
+        if gender == Constants.MALE:
             gender_tail = '(муж.)'
         else:
             gender_tail = '(жен.)'
     noble_tail = ''
-    if nobility == 'R':
+    if nobility == Constants.RANDOM:
         nobility = determine_nobility(nobility)
         gender_tail += ' '
-        if nobility == 'N':
+        if nobility == Constants.NOBLE:
             noble_tail = u'(благородное)'
         else:
             noble_tail = u'(простое)'
@@ -302,7 +423,7 @@ def resolve_randomness(gender, nobility):
 
 
 def generate_cognomen(gender, first_parts, male, female):
-    if gender == 'M':
+    if gender == Constants.MALE:
         return CognomenLatin.objects.get_random_cognomen(first_parts, male)
     else:
         return CognomenLatin.objects.get_random_cognomen(first_parts, female)
@@ -311,7 +432,7 @@ def generate_cognomen(gender, first_parts, male, female):
 def generate_name(lang_male, lang_female, gender,
                   first_parts_male, second_parts_male,
                   first_parts_female, second_parts_female):
-    if gender == 'M':
+    if gender == Constants.MALE:
         name = lang_male.objects.get_random_name(first_parts_male, second_parts_male)
     else:
         name = lang_female.objects.get_random_name(first_parts_female, second_parts_female)
@@ -327,7 +448,7 @@ def generate_scand_surname(gender, first_parts_male, second_parts_male,
     else:
         surname = FemaleScand.objects.get_random_name(first_parts_female, second_parts_female)
     dice = d100()
-    if gender == 'M':
+    if gender == Constants.MALE:
         if dice <= 50:
             surname += 'ссон'
         else:
@@ -343,7 +464,7 @@ def generate_scand_surname(gender, first_parts_male, second_parts_male,
 def generate_spanish_surname(nobility, first_parts_male, second_parts_male,
                              first_parts_female, second_parts_female):
     surname = ''
-    if nobility == 'N':
+    if nobility == Constants.NOBLE:
         threshold = 75
     else:
         threshold = 50
@@ -427,7 +548,7 @@ def generate_spanish_surname(nobility, first_parts_male, second_parts_male,
             else:
                 surname += 'ия'
     else:
-        if nobility == 'N':
+        if nobility == Constants.NOBLE:
             if d100() <= 33:
                 surname = ' ла ' + surname
             elif d100() <= 67:
@@ -440,7 +561,7 @@ def generate_spanish_surname(nobility, first_parts_male, second_parts_male,
 def generate_italian_surname(nobility, first_parts, second_parts):
     surname = ''
     surname = SurnamesItalian.objects.get_random_name(first_parts, second_parts)
-    if nobility == 'N':
+    if nobility == Constants.NOBLE:
         if d100() <= 50:
             if surname[1:].lower() in VOWELS:
                 if d100() <= 50:
