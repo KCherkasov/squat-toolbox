4b9da6884035a5ec6dd6abde87940bfb89df35c2
Kirill Cherkasov <kvcherk@live.ru>
2022-03-11 16:02:07 +0300

    minor fixup

--- namegen/namegen/urls.py
+++ namegen/namegen/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from . import views, settings
 
 urlpatterns = [
-    path(r'$', views.main, name='main'),
-    path(r'admin/$', admin.site.urls),
-    path(r'namegen/$', views.index, name='index'),
+    path(r'', views.main, name='main'),
+    path(r'admin/', admin.site.urls),
+    path(r'namegen/', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
