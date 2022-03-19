e36190c1eebbef89ae8eadc07da190415bd6dc4e
Kirill Cherkasov <kvcherk@live.ru>
2022-03-19 14:31:17 +0300

    minor fixup

--- namegen/settings.py
+++ namegen/settings.py
@@ -72,7 +72,6 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
 
