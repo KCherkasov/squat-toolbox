41a9c644aa3fdc4ddab6c313b9ab89b9bd6a411f
Kirill Cherkasov <kvcherk@live.ru>
2022-03-24 20:27:42 +0300

    minor fixup

--- namegen/settings.py
+++ namegen/settings.py
@@ -79,6 +79,7 @@ REST_FRAMEWORK = {
 
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = None
+CSRF_TRUSTED_ORIGINS = ['https://wiki.pandhammer.ru', 'https://kvcherk.pythonanywhere.com/']
 
 
 CSP_FRAME_ANCESTORS = ['https://wiki.pandhammer.ru', 'https://kvcherk.pythonanywhere.com/']
