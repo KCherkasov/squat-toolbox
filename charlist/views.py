2dc5ccc1f11a38a4b481cbc0f92aafa0b140e82f
Kirill Cherkasov <kvcherk@live.ru>
2022-04-25 20:03:50 +0300

    charlist development start

--- charlist/views.py
+++ charlist/views.py
@@ -2,14 +2,14 @@
 import sys
 
 from django.shortcuts import render, reverse
-
+import logging
 
 from .flyweights import *
 from .constants import *
 
 data = json.load(open('static/json/aptitudes.json', 'r'))
 aptitudes = Aptitude.from_file(data)
-print(data, file=sys.stderr)
+logging.debug(data)
 
 
 def aptitudes_test(request):
