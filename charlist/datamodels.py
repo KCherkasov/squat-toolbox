ba6f2b37adb3e0fbe1b0969a9333a43810961226
Kirill Cherkasov <kvcherk@live.ru>
2022-07-12 18:09:18 +0300

    charsheet dev commit -- resource files filling

--- charlist/datamodels.py
+++ charlist/datamodels.py
@@ -48,12 +48,14 @@ class SkillDescriptionModel(object):
 
 class HintModel(object):
     def __init__(self, tag: str, description: Dict[str, str], targets: List[str],
-                 bonus: int = None, condition: str = None):
+                 bonus: int = None, condition: str = None, base: float = None, command: Dict = None):
         self.tag = tag
         self.description = description
         self.targets = targets
         self.bonus = bonus
         self.condition = condition
+        self.base = base
+        self.command = command
 
     @classmethod
     def from_json(cls, data):
