77f1e4dc91c1d35507ee54a220c5a701c811109f
Kirill Cherkasov <kvcherk@live.ru>
2022-07-21 00:39:40 +0300

    charsheet dev commit -- resource files filling -- interactive mockup test -- widgets

--- namegen/settings.py
+++ namegen/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'namegen',
-    'character',
+    'charlist',
     'rest_framework',
     'corsheaders',
 ]
