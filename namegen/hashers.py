5aba7b496a467d488671ca485a6428e229e4fcda
Kirill Cherkasov <kvcherk@live.ru>
2022-07-22 01:01:18 +0300

    charsheet dev commit -- resource files filling -- interactive mockup test -- widgets

--- namegen/hashers.py
+++ namegen/hashers.py
@@ -4,4 +4,4 @@ from django.contrib.auth.hashers import PBKDF2PasswordHasher
 
 
 class CustomPBKDF2PasswordHasher(PBKDF2PasswordHasher):
-    iterations = PBKDF2PasswordHasher.iterations * 100
+    iterations = PBKDF2PasswordHasher.iterations * 50
