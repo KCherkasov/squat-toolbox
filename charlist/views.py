19915d021b3196ae40671067b545641003e57457
Kirill Cherkasov <kvcherk@live.ru>
2023-01-31 20:58:17 +0300

    charsheet dev commit -- resource files filling -- interactive mockup test -- widgets

--- charlist/views.py
+++ charlist/views.py
@@ -546,7 +546,7 @@ def create_character_choices(request, creation_id):
                 return HttpResponseRedirect(reverse('create-character-double-apts', kwargs={'creation_id': cd.pk}))
             else:
                 return render(request, 'character_creation_form.html', {'version': VERSION, 'facade': flyweights,
-                                                                        'stage': CREATION_STAGES[5], 'form': form}
+                                                                        'stage': CREATION_STAGES[5], 'form': form})
     else:
         form = ChoicesForm()
         form = prepare_choices_form(form, cd)
