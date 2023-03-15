dbd53039b4e9798de70472556c89fb719f0b0746
Kirill Cherkasov <kvcherk@live.ru>
2023-03-15 20:29:54 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/flyweights/models/malignance_model.py
+++ charlist/flyweights/models/malignance_model.py
@@ -5,13 +5,13 @@ from charlist.flyweights.models.hint_model import HintModel
 
 class MalignanceModel(object):
     def __init__(self, tag: str, name: Dict[str, str], description: Dict[str, str], hints: List[HintModel],
-                 commands: List[Dict], roll_range: List[int]):
+                 commands: List[Dict], rolls_range: List[int]):
         self.tag = tag
         self.name = name
         self.description = description
         self.hints = hints
         self.commands = commands
-        self.roll_range = roll_range
+        self.rolls_range = rolls_range
 
     @classmethod
     def from_json(cls, data):
