c773285a24d3f7c1e01f473d6b139878c33b571c
Kirill Cherkasov <kvcherk@live.ru>
2023-03-19 00:19:21 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/player_todos/manual/increase_stat_alt_form.py
+++ charlist/forms/player_todos/manual/increase_stat_alt_form.py
@@ -7,8 +7,8 @@ from charlist.flyweights.flyweights import Facade
 class IncreaseStatAltForm(Form):
     def __init__(self, cmd, flyweights: Facade, *args, **kwargs):
         super(IncreaseStatAltForm, self).__init__(*args, **kwargs)
-        choices = [(cmd.get('tag1', flyweights.stat_descriptions().get(cmd.get('tag1')).get_name_en())),
-                   (cmd.get('tag2', flyweights.stat_descriptions().get(cmd.get('tag2')).get_name_en()))]
+        choices = [(cmd.get('tag1'), flyweights.stat_descriptions().get(cmd.get('tag1')).get_name_en()),
+                   (cmd.get('tag2'), flyweights.stat_descriptions().get(cmd.get('tag2')).get_name_en())]
         self.__amount = cmd.get('amount')
         self.fields['choices'].choices = choices
 
