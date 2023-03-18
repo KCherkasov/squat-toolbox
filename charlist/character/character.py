2327765c6bbad42801fbfe366b5ad50af7d94527
Kirill Cherkasov <kvcherk@live.ru>
2023-03-18 22:23:29 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/character/character.py
+++ charlist/character/character.py
@@ -507,8 +507,8 @@ class CharacterModel(object):
             talents[talent_key] = Talent.from_json(talent)
         data['talents'] = talents
         traits = dict()
-        for trait_key, trait in data['traits'].items():
-            traits[trait_key] = Trait.from_json(trait)
+        for trait_key in data['traits']:
+            traits[trait_key] = Trait.from_json(data['traits'].get(trait_key))
         data['traits'] = traits
         return cls(**data)
 
--- charlist/constants/constants.py
+++ charlist/constants/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-VERSION = '0.0.124a'
+VERSION = '0.0.125a'
 
 RU = 'ru'
 EN = 'en'
