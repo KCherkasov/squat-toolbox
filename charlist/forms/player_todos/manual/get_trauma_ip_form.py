5d7781277d86de64139e7419285ad8e162e86f89
Kirill Cherkasov <kvcherk@live.ru>
2023-03-19 04:22:08 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/player_todos/manual/get_trauma_ip_form.py
+++ charlist/forms/player_todos/manual/get_trauma_ip_form.py
@@ -5,16 +5,14 @@ from django.forms import Form
 class GetTraumaIPForm(Form):
     def __int__(self, cmd: dict = None, *args, **kwargs):
         super().__int__(*args, **kwargs)
-        self.__cmd_id = -1
+        self.cmd_id = -1
         if cmd is not None:
-            self.__cmd_id = cmd.get('cmd_id')
+            self.cmd_id = cmd.get('cmd_id')
 
-    def cmd_id(self):
-        return self.__cmd_id
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        cleaned_data['cmd_id'] = self.__cmd_id
+        cleaned_data['cmd_id'] = self.cmd_id
         return cleaned_data
 
     roll_result = forms.IntegerField(min_value=1, max_value=210, required=False,
