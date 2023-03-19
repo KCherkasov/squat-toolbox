47b4453752bd7540efba18fd56879ef206050dd8
Kirill Cherkasov <kvcherk@live.ru>
2023-03-19 05:04:23 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/player_todos/manual/decrease_stat_alt_form.py
+++ charlist/forms/player_todos/manual/decrease_stat_alt_form.py
@@ -7,7 +7,7 @@ from charlist.flyweights.flyweights import Facade
 class DecreaseStatAltForm(Form):
     def __init__(self, cmd: dict, flyweights: Facade, *args, **kwargs):
         super(DecreaseStatAltForm, self).__init__(*args, **kwargs)
-        choices = [(cmd.get('tag1'), flyweights.stat_descriptions().get(cmd.get('tag2')).get_name_en()),
+        choices = [(cmd.get('tag1'), flyweights.stat_descriptions().get(cmd.get('tag1')).get_name_en()),
                    (cmd.get('tag2'), flyweights.stat_descriptions().get(cmd.get('tag2')).get_name_en())]
         self.fields['choices'].choices = choices
         self.__amount = cmd.get('amount')
