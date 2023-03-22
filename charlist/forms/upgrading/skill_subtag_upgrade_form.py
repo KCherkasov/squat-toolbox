c2bb846701d4e6d6bd62a22f62b4aed0f0b26771
Kirill Cherkasov <kvcherk@live.ru>
2023-03-22 22:43:34 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/upgrading/skill_subtag_upgrade_form.py
+++ charlist/forms/upgrading/skill_subtag_upgrade_form.py
@@ -6,7 +6,7 @@ class SkillSubtagUpgradeForm(Form):
     def __init__(self, sk_tag: str, subtag: str, cost: int, advance: int, *args, **kwargs):
         super(SkillSubtagUpgradeForm, self).__init__(*args, **kwargs)
         self.__sk_tag = sk_tag
-        self.__subtag = subtag
+        self.__subtag_skill = subtag
         self.__cost = cost
         self.__advance = advance
         if subtag == 'SK_ANY':
@@ -15,8 +15,8 @@ class SkillSubtagUpgradeForm(Form):
     def skill_tag(self):
         return self.__sk_tag
 
-    def subtag(self):
-        return self.__subtag
+    def subtag_skill(self):
+        return self.__subtag_skill
 
     def cost(self):
         return self.__cost
