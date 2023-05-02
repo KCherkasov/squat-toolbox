1669e7c33f1bb5380d835b6c490700f149c845a9
Kirill Cherkasov <kvcherk@live.ru>
2023-05-02 14:05:52 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/rt_views.py
+++ charlist/rt_views.py
@@ -101,7 +101,7 @@ def rt_create_character_oac_choice(request, creation_id):
                 cdm.lure_id = form.cleaned_data['lure_id']
                 cdm.trial_id = form.cleaned_data['trial_id']
                 cdm.motivation_id = form.cleaned_data['motivation_id']
-                cdm.full_reset()
+                cdm.full_reset(rt_flyweights)
                 cdm.xp[0] += rt_flyweights.birthrights().get(cdm.birthright_id).cost()
                 cdm.xp[0] += rt_flyweights.lures().get(cdm.lure_id).cost()
                 cdm.xp[0] += rt_flyweights.trials().get(cdm.trial_id).cost()
