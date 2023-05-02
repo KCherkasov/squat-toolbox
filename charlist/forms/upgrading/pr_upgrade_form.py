adbb71e3c31e51a682d75f7ded7052c8d5644963
Kirill Cherkasov <kvcherk@live.ru>
2023-05-02 22:13:00 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/upgrading/pr_upgrade_form.py
+++ charlist/forms/upgrading/pr_upgrade_form.py
@@ -4,14 +4,8 @@ from django.forms import Form
 class PRUpgrageForm(Form):
     def __init__(self, pr: int, cost: int, *args, **kwargs):
         super(PRUpgrageForm, self).__init__(*args, **kwargs)
-        self.__pr = pr
-        self.__cost = cost
-
-    def pr(self):
-        return self.__pr
+        self.pr = pr
+        self.cost = cost
 
     def pr_new(self):
-        return self.__pr + 1
-
-    def cost(self):
-        return self.__cost
+        return self.pr + 1
