905821a272c3a15618b6d8c09bec54563ccba08b
Kirill Cherkasov <kvcherk@live.ru>
2023-05-04 02:29:31 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/generation/rt/choices_form.py
+++ charlist/forms/generation/rt/choices_form.py
@@ -74,6 +74,7 @@ class RTChoicesForm(Form):
 
     def clean(self):
         cd = super(RTChoicesForm, self).clean()
+        log = str(cd)
         for field_name, value in cd.items():
             if field_name[:len(STG_PREFIX)] == STG_PREFIX:
                 stripped_name = field_name[len(STG_PREFIX):]
@@ -92,6 +93,7 @@ class RTChoicesForm(Form):
                     err = 'Wrong-spawned text field: ' + field_name + ' has no '\
                           + stripped_name + ' match in form fields'
                     raise ValidationError(err)
+        raise AttributeError({'log': log})
         return cd
 
 
--- charlist/rt_views.py
+++ charlist/rt_views.py
@@ -178,20 +178,25 @@ def rt_create_character_choices(request, creation_id):
             if form.is_valid():
                 cleaned_data = form.cleaned_data
                 cdm.choices = list()
+                log = str(cleaned_data)
                 for i in range(len(cleaned_data)):
                     field_name = 'choice-' + str(i)
+                    log += field_name + ': '
                     if field_name not in cleaned_data.keys():
                         break
                     choice = json.loads(cleaned_data.get(field_name))
+                    log += cleaned_data.get(field_name) + ' - tag: ' + choice.get('tag')
                     cdm.choices.append(choice)
                     if choice.get('tag')[:2] == 'A_':
                         cdm.aptitudes.append(choice.get('tag'))
                     elif choice.get('tag')[:2] == 'SK':
                         if 'subtag' in choice.keys():
+                            log += ' subtag: ' + choice.get('subtag')
                             if choice.get('subtag') == 'SK_ANY':
                                 subtag_fld_name = str(STG_PREFIX) + field_name
                                 res_choice = {'tag': choice.get('tag'),
                                               'subtag': cleaned_data.get(subtag_fld_name)}
+                                log += ': ' + cleaned_data.get(subtag_fld_name)
                                 cdm.skills.append(res_choice)
                             else:
                                 cdm.skills.append(choice)
@@ -215,6 +220,7 @@ def rt_create_character_choices(request, creation_id):
                             cmd['tag'] = choice.get('stat')
                             cmd['amount'] = choice.get('amount')
                         cdm.commands.append(cmd)
+                raise AttributeError({'log': log})
                 cd.curr_stage = 'double-apts'
                 cd.last_mod_date = datetime.datetime.now()
                 cd.character_data = cdm.to_json()
