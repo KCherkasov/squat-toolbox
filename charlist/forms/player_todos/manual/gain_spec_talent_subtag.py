755a247e027ab7895490954c0ed5e5ac3980b4ae
Kirill Cherkasov <kvcherk@live.ru>
2023-05-04 15:04:06 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/player_todos/manual/gain_spec_talent_subtag.py
+++ charlist/forms/player_todos/manual/gain_spec_talent_subtag.py
@@ -5,7 +5,7 @@ from django.forms import Form
 class GainSpecTalentForm(Form):
     def __init__(self, flyweights, cmd: dict, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.sk_tag = cmd.get('tag')
+        self.tl_tag = cmd.get('tag')
         self.cmd_id = cmd.get('cmd_id')
         self.tl_name = flyweights.talent_descriptions().get(cmd.get('tag')).name().get('en')
 
