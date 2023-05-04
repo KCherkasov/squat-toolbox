98e50288f5c6f5750b6a74d9caf7c828c316b983
Kirill Cherkasov <kvcherk@live.ru>
2023-05-04 17:44:19 +0300

    charsheet dev commit -- commands system implementation -- new widgets

--- charlist/forms/player_todos/manual/decrease_stat_alt_form.py
+++ charlist/forms/player_todos/manual/decrease_stat_alt_form.py
@@ -5,8 +5,13 @@ from django.forms import Form
 class DecreaseStatAltForm(Form):
     def __init__(self, cmd: dict, flyweights, *args, **kwargs):
         super(DecreaseStatAltForm, self).__init__(*args, **kwargs)
-        choices = [(cmd.get('tag1'), flyweights.stat_descriptions().get(cmd.get('tag1')).get_name_en()),
-                   (cmd.get('tag2'), flyweights.stat_descriptions().get(cmd.get('tag2')).get_name_en())]
+        if 'tag1' in cmd.keys():
+            choices = [(cmd.get('tag1'), flyweights.stat_descriptions().get(cmd.get('tag1')).get_name_en()),
+                       (cmd.get('tag2'), flyweights.stat_descriptions().get(cmd.get('tag2')).get_name_en())]
+        else:
+            choices = list()
+            for tag in cmd.get('tags'):
+                choices.append((tag, flyweights.stat_descriptions().get(tag).get_name_en()))
         self.fields['choices'].choices = choices
         self.__amount = cmd.get('amount')
         self.__cmd_id = cmd.get('cmd_id')
