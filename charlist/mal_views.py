e356ffbbac0bfb8a17dce3179933f6a55d8c2e2a
Kirill Cherkasov <kvcherk@live.ru>
2024-03-25 01:37:11 +0300

    Imperium Maledictum charsheet: -- added skills widget -- fixed bug with invalid skill advances displaying and counting

--- charlist/mal_views.py
+++ charlist/mal_views.py
@@ -32,9 +32,10 @@ def mal_character_mock_view(request):
         specs = dict()
         skills[skill_tag] = MalSkill(skill_tag, int(rnd.randrange(IM_ZERO, IM_SKILL_ADV_CAP)), specs)
     for spec_tag, descr in mal_flyweights.specialization_descriptions().items():
+        parent = descr.parent
         spec = MalSpecialization(spec_tag, int(rnd.randrange(IM_ZERO, IM_SKILL_ADV_CAP)))
         if spec.advances() > IM_ZERO:
-            skills.get(descr.parent).gain_specialization(spec)
+            skills.get(parent).gain_specialization(spec)
     wounds = (stats.get(ST_STRENGTH).bonus() + stats.get(ST_TOUGHNESS).bonus()
               + stats.get(ST_TOUGHNESS).bonus() + stats.get(ST_WILLPOWER).bonus())
     character_model = MalCharacterModel(IM_ZERO, "Test Maledictum Character", [100, 0], [3, 3],
