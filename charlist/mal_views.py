143040956b5d3937c7d4fc8a242d277fdaef6276
Kirill Cherkasov <kvcherk@live.ru>
2024-03-24 10:49:26 +0300

    Imperium Maledictum initial commit -- resources: ---- stats, skills and specializations descriptions; -- mechanics: ---- stats, skills and specialization in character model -- charsheet mockup view: ---- stats widget preview

--- charlist/mal_views.py
+++ charlist/mal_views.py
@@ -27,7 +27,7 @@ def mal_character_mock_view(request):
     rnd = Random(datetime.datetime.now())
     for stat_id in STAT_TAGS_GEN:
         stats[stat_id] = MalStat(stat_id, IM_BASE_STAT + int(rnd.random() % IM_D10 + IM_ONE)
-                                 + int(rnd.random() % IM_D10 + IM_ONE, IM_ZERO))
+                                 + int(rnd.random() % IM_D10 + IM_ONE), IM_ZERO)
     skills = dict()
     character_model = MalCharacterModel(IM_ZERO, "Test Maledictum Character", [100, 0], [3, 3],
                                         [10, 10], stats, skills)
